99ecec38ac5fdf35299c512de4fc09307508dfc1
Matt McLimans <mmclimans@paloaltonetworks.com>
2024-07-11 20:25:00 -0400

    Create app_startup.sh

---
+++ scripts/app_startup.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+echo "while :" >> /network-check.sh
+echo "do" >> /network-check.sh
+echo "  timeout -k 2 2 ping -c 1  8.8.8.8 >> /dev/null" >> /network-check.sh
+echo "  if [ $? -eq 0 ]; then" >> /network-check.sh
+echo "    echo \$(date) -- Online -- Source IP = \$(curl https://checkip.amazonaws.com -s --connect-timeout 1)" >> /network-check.sh
+echo "  else" >> /network-check.sh
+echo "    echo \$(date) -- Offline" >> /network-check.sh
+echo "  fi" >> /network-check.sh
+echo "  sleep 1" >> /network-check.sh
+echo "done" >> /network-check.sh
+chmod +x /network-check.sh
+
+while ! ping -q -c 1 -W 1 google.com >/dev/null
+do
+  echo "waiting for internet connection..."
+  sleep 10s
+done
+echo "internet connection available!"
+sudo apt-get install php -y
+sudo rm -f /var/www/html/index.html
+sudo wget -O /var/www/html/index.php https://raw.githubusercontent.com/wwce/terraform/master/azure/transit_2fw_2spoke_common/scripts/showheaders.php
+sudo systemctl restart apache2
